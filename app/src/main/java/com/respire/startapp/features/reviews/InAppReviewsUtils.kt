d709e92da38d93b353baf85c13ea02c276e05b74
Nikita Skubak <nikskubak@gmail.com>
2021-06-17 22:12:52 +0300

    reviews

--- app/src/main/java/com/respire/startapp/features/reviews/InAppReviewsUtils.kt
+++ app/src/main/java/com/respire/startapp/features/reviews/InAppReviewsUtils.kt
@@ -1,9 +1,17 @@
 package com.respire.startapp.features.reviews
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import com.google.android.play.core.review.ReviewManagerFactory
 import com.google.android.play.core.review.testing.FakeReviewManager
+import com.respire.startapp.BuildConfig
+import com.respire.startapp.R
+import kotlinx.android.synthetic.main.dialog_rate.view.*
 
 object InAppReviewHelper {
     fun reviewApp(
@@ -24,6 +32,7 @@ object InAppReviewHelper {
             } else {
                 task.exception?.printStackTrace()
                 onCompleteListener(false)
+                openAppInGooglePlay(context)
             }
         }
     }
@@ -45,7 +54,69 @@ object InAppReviewHelper {
             } else {
                 task.exception?.printStackTrace()
                 onCompleteListener(false)
+                openAppInGooglePlay(context)
             }
+        }
+    }
+
+    public fun showReviewDialog(
+        context: Context,
+        activity: Activity,
+        onCompleteListener: (isSuccess: Boolean) -> Unit
+    ) {
+        val builder: AlertDialog.Builder =
+            AlertDialog.Builder(context, R.style.CustomDialog)
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_rate, null, false)
+        builder.setView(view)
+        val dialog: AlertDialog = builder.create()
+        view.rateButton.setOnClickListener {
+            fakeReviewApp(context, activity, onCompleteListener)
+            dialog.dismiss()
+        }
+        view.problemButton.setOnClickListener {
+            openMailClient(activity)
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun openAppInGooglePlay(context: Context) {
+        try {
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=${BuildConfig.APPLICATION_ID}")
+                )
+            )
+        } catch (anfe: ActivityNotFoundException) {
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}")
+                )
+            )
+        }
+    }
+
+    private fun openMailClient(activity: Activity) {
+        try {
+//            ShareCompat.IntentBuilder(activity)
+//                .setType("text/plain")
+//                .addEmailTo("respirecorp@gmail.com")
+//                .setSubject("${activity.getString(R.string.app_trouble)} \"${activity.getString(R.string.app_name)}\"")
+//                .setChooserTitle(activity.getString(R.string.mail_client))
+//                .startChooser()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // only email apps should handle this
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("respirecorp@gmail.com"))
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    "${activity.getString(R.string.app_trouble)} \"${activity.getString(R.string.app_name)}\""
+                )
+            }
+            activity.startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
--- app/src/main/java/com/respire/startapp/ui/MainActivity.kt
+++ app/src/main/java/com/respire/startapp/ui/MainActivity.kt
@@ -6,8 +6,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.format.DateUtils
 import android.util.Log
-import android.view.LayoutInflater
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -20,7 +18,6 @@ import com.respire.startapp.features.notifications.NotificationScheduler
 import com.respire.startapp.features.reviews.InAppReviewHelper
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.dialog_rate.view.*
 import java.util.*
 import javax.inject.Inject
 
@@ -43,79 +40,57 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         viewModel = ViewModelProvider(this, vmFactory).get(MainViewModel::class.java)
         retrieveEntities()
         showNotification()
-        showRateDialog()
-    }
-
-    private fun showReview() {
-        InAppReviewHelper.fakeReviewApp(this, this) {
+        InAppReviewHelper.showReviewDialog(this, this) {
             Log.e("InAppReviewHelper", it.toString())
         }
     }
 
-    private fun showRateDialog() {
-        val builder: AlertDialog.Builder =
-            AlertDialog.Builder(this, R.style.CustomDialog)
-        val view = LayoutInflater.from(this).inflate(R.layout.dialog_rate, null, false)
-        builder.setView(view)
-        val dialog: AlertDialog = builder.create()
-        view.rateButton.setOnClickListener {
-//            openAppInGooglePlay()
-            showReview()
-            dialog.dismiss()
-        }
-        view.problemButton.setOnClickListener {
-//            openMailClient()
-            dialog.dismiss()
-        }
-        dialog.show()
+    private fun showNotification() {
+        NotificationScheduler.Builder(this, "unique notification id")
+            .title("Test title")
+            .description("Test description")
+            .icon(R.drawable.ic_launcher_foreground)
+            .channelId("Notifications about events")
+            .notificationDate(Date().apply {
+                time = System.currentTimeMillis() + DateUtils.MINUTE_IN_MILLIS
+            })
+            .schedule()
     }
 
-private fun showNotification() {
-    NotificationScheduler.Builder(this, "unique notification id")
-        .title("Test title")
-        .description("Test description")
-        .icon(R.drawable.ic_launcher_foreground)
-        .channelId("Notifications about events")
-        .notificationDate(Date().apply {
-            time = System.currentTimeMillis() + DateUtils.MINUTE_IN_MILLIS
+    private fun retrieveEntities() {
+        swipeRefreshLayout.isRefreshing = true
+        viewModel.getEntities().observe(this, Observer<Result<MutableList<Entity>>> {
+            it.data?.let { data ->
+                adapter.data = data
+                adapter.notifyDataSetChanged()
+            }
+            it.error?.printStackTrace()
+            swipeRefreshLayout.isRefreshing = false
         })
-        .schedule()
-}
 
-private fun retrieveEntities() {
-    swipeRefreshLayout.isRefreshing = true
-    viewModel.getEntities().observe(this, Observer<Result<MutableList<Entity>>> {
-        it.data?.let { data ->
-            adapter.data = data
-            adapter.notifyDataSetChanged()
-        }
-        it.error?.printStackTrace()
-        swipeRefreshLayout.isRefreshing = false
-    })
-
-}
-
-private fun initViews() {
-    entitiesRecyclerView.layoutManager = layoutManager
-    entitiesRecyclerView.adapter = adapter
-    swipeRefreshLayout.setOnRefreshListener { retrieveEntities() }
-}
-
-private fun openAppInGooglePlay(it: String?) {
-    try {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("market://details?id=$it")
-            )
-        )
-    } catch (anfe: ActivityNotFoundException) {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("https://play.google.com/store/apps/details?id=$it")
-            )
-        )
     }
-}
+
+    private fun initViews() {
+        entitiesRecyclerView.layoutManager = layoutManager
+        entitiesRecyclerView.adapter = adapter
+        swipeRefreshLayout.setOnRefreshListener { retrieveEntities() }
+    }
+
+    private fun openAppInGooglePlay(it: String?) {
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=$it")
+                )
+            )
+        } catch (anfe: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$it")
+                )
+            )
+        }
+    }
 }
